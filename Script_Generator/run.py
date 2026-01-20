776eefe20d6b15d5970d9fd6653249c8b14eca2f
Ahmad Nazir <ahmadnazir9101@gmail.com>
2026-01-21 00:22:11 +0500

    Initialize database in app context
    Add database initialization within app context.

--- Script_Generator/run.py
+++ Script_Generator/run.py
@@ -2,6 +2,7 @@ from ai_script_generator import create_app
 from flask import Flask
 
 app=create_app()
-
+with app.app_context():
+    db.create_all()
 if __name__=='__main__':
     app.run(debug=True)
